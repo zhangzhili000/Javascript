a0a01b06fa3055f98a58f5c1a7f3e594cb0d9187
zhangzhili <zhangz_l@163.com>
2018-04-20 20:27:45 +0200

    Array.map()_code

---
+++ notebook_examples/map_function.js
@@ -0,0 +1,32 @@
+// /**
+// * when the elements are numbers.
+// */
+
+// let arr1 = [];
+// arr1 = [1,2,3,4,5,6]
+// let arr2 = arr1.map(function (val) {
+// 	return val;
+// });
+// console.log("arr1:",arr1,"\n arr2:",arr2);
+// arr2[3] = 10;
+// console.log("arr1:",arr1,"\n arr2:",arr2);
+
+
+
+/**
+* when the elements are objects.
+*/
+let obj1 = {name:"kitty",age:50,type:"toy"};
+let obj2 = {init:function(){return this.age;},age:30,type:"function"};
+let obj3 = {fun:function(a,b){return a+b;},type:"operation"};
+let arr1 = [];
+arr1 = [obj1,obj2,obj3];
+let arr2 = arr1.map(function (val) {
+	return val;
+});
+console.log("arr1:",arr1,"\n", "arr2:",arr2);
+arr2[2].fun = function(a,b){return a-b;};
+console.log("arr1:",arr1,"\n", "arr2:",arr2);
+
+console.log("arr1[2].fun:",arr1[2].fun(2,3),"\n", "arr2[2].fun:",arr2[2].fun(2,3));
+
