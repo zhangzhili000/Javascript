8d4b3b742a6f30c727c2737ab9c52755f9f2dba3
zhangzhili <zhangz_l@163.com>
2018-07-05 15:43:07 +0200

    Add a dp method

--- leetCode/62_Unique_Paths.js
+++ leetCode/62_Unique_Paths.js
@@ -62,6 +62,35 @@ var uniquePaths = function(m, n) {
 
 
 
+解决方案三：动态规划（打败91.13%）
+
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+    let res = [];
+    for(let i = 0;i<m;i++){
+        res[i] = [];
+        for(let j = 0;j<n;j++){
+            if(i==0 && j==0){
+                res[i][j] = 1;
+            }else if(i==0){
+                res[i][j] = res[i][j-1];
+            }else if(j==0){
+                res[i][j] = res[i-1][j];
+            }else{
+                res[i][j] = res[i-1][j] + res[i][j-1];
+            }
+        }
+    }
+    return res[m-1][n-1];
+};
+
+
+
+
 
 Testcases:
 3
